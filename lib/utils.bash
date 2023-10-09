20fb734d5e5bef5fe92c276b0ac5d4c258a13361
nyuyuyu <127820811+nyuyuyu@users.noreply.github.com>
2023-10-09 19:56:13 +0900

    Improve `list-all`

--- lib/utils.bash
+++ lib/utils.bash
@@ -4,6 +4,7 @@ set -euo pipefail
 
 # TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for flutter.
 GH_REPO="https://github.com/flutter/flutter"
+FLUTTER_LIST_BASE_URL="https://storage.googleapis.com"
 TOOL_NAME="flutter"
 TOOL_TEST="flutter --help"
 
@@ -12,28 +13,61 @@ fail() {
 	exit 1
 }
 
-curl_opts=(-fsSL)
+platform() {
+	case "$(uname -s)" in
+	"Darwin")
+		echo "macos"
+		;;
+	"Linux")
+		echo "linux"
+		;;
+	*)
+		fail "Unsupported platform"
+		;;
+	esac
+}
 
-# NOTE: You might want to remove this if flutter is not hosted on GitHub releases.
-if [ -n "${GITHUB_API_TOKEN:-}" ]; then
-	curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
-fi
+machine_architecture() {
+	case "$(uname -m)" in
+	"x86_64")
+		echo "x64"
+		;;
+	"arm64")
+		echo "arm64"
+		;;
+	*)
+		fail "Unsupported archtecture"
+		;;
+	esac
+}
+
+flutter_download_list_url() {
+	echo "$FLUTTER_LIST_BASE_URL/flutter_infra_release/releases/releases_$(platform).json"
+}
+
+curl_opts=(-fsSL)
 
 sort_versions() {
 	sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
 		LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
 }
 
-list_github_tags() {
-	git ls-remote --tags --refs "$GH_REPO" |
-		grep -o 'refs/tags/.*' | cut -d/ -f3- |
-		sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
+list_released_versions() {
+	curl "${curl_opts[@]}" "$(flutter_download_list_url)" | jq -r '.releases[].version |= gsub("^v"; "") | .releases'
+}
+
+list_filter_by_archtecture() {
+	local arch="$1"
+	local query=".[] | select(.dart_sdk_arch == \"$arch\")"
+	if [ "$arch" == "x64" ]; then
+		query=".[] | select(.dart_sdk_arch == \"$arch\" or (has(\"dart_sdk_arch\") | not))"
+	fi
+
+	list_released_versions | jq -r "$query"
 }
 
 list_all_versions() {
-	# TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-	# Change this function if flutter has other means of determining installable versions.
-	list_github_tags
+	list_filter_by_archtecture "$(machine_architecture)" | jq -r '.version + "-" + .channel'
 }
 
 download_release() {
